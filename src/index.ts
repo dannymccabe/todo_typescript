6c32cc4c79e74f32be32cc09442b5538423348e4
Danny <danny.affiliatemarketing@gmail.com>
2022-04-29 19:33:13 -0300

    updated and finished todo list writen in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,41 @@
-import { v4 as uuidV4 } from 'uuid';
-type Task = {
-  id: string;
-  title: string;
-  completed: boolean;
-  createdAt: Date;
-};
+import { v4 as uuidV4 } from "uuid"
 
-const list = document.querySelector<HTMLUListElement>('#list');
-const form = document.getElementById('new-task-form') as HTMLFormElement | null;
-const input = document.querySelector<HTMLInputElement>('#new-task-item');
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
 const tasks: Task[] = loadTasks()
 tasks.forEach(addListItem)
 
-form?.addEventListener('submit', (e) => {
-  e.preventDefault();
+form?.addEventListener("submit", e => {
+  e.preventDefault()
 
-  if (input?.value === '' || input?.value == null) return;
+  if (input?.value == "" || input?.value == null) return
 
   const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date(),
-  };
+  }
   tasks.push(newTask)
+  saveTasks()
 
-  addListItem(newTask);
-  input.value = ''
-});
+  addListItem(newTask)
+  input.value = ""
+})
 
-function addListItem(task: Task){
-  const item = document.createElement('li')
-  const label = document.createElement('label')
-  const checkbox = document.createElement('input')
-  checkbox.addEventListener('change', () => {
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
     task.completed = checkbox.checked
     saveTasks()
   })
@@ -42,7 +44,7 @@ function addListItem(task: Task){
   label.append(checkbox, task.title)
   item.append(label)
   list?.append(item)
-};
+}
 
 function saveTasks() {
   localStorage.setItem("TASKS", JSON.stringify(tasks))
@@ -50,6 +52,6 @@ function saveTasks() {
 
 function loadTasks(): Task[] {
   const taskJSON = localStorage.getItem("TASKS")
-  if(taskJSON == null) return []
+  if (taskJSON == null) return []
   return JSON.parse(taskJSON)
 }
